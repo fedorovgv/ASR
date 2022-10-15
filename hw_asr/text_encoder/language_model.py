3e921f661b0adefd9b62ae8a14b72e3017755a80
fedorovgv <ivlog7@yandex.ru>
2022-10-15 23:58:43 +0300

    Add lm model download script

---
+++ hw_asr/text_encoder/language_model.py
@@ -0,0 +1,44 @@
+#  from Nemo library "NeMo offline ASR" notebook example
+
+import logging
+import os, shutil
+from pathlib import Path
+from speechbrain.utils.data_utils import download_file
+
+logger = logging.getLogger(__name__)
+
+AVAILABLE_MODELS = [
+    '3-gram.arpa',
+    '3-gram.pruned.1e-7.arpa',
+    '3-gram.pruned.3e-7.arpa',
+    '4-gram.arpa',
+]
+
+
+def load_model(lm_dir: Path, lm_model: str):
+    assert lm_model in AVAILABLE_MODELS
+
+    lm_gzip_path = lm_model + '.gz'
+
+    if not os.path.exists(lm_gzip_path):
+        logger.info(f'Downloading language model in {lm_dir}')
+        lm_url = 'http://www.openslr.org/resources/11/' + lm_gzip_path
+        download_file(lm_url, lm_dir / lm_model)
+
+    uppercase_lm_path = lm_dir / lm_model
+    if not os.path.exists(uppercase_lm_path):
+        with open(lm_gzip_path, 'rb', encoding='utf-8') as f_zipped:
+            with open(uppercase_lm_path, 'wb', encoding='utf-8') as f_unzipped:
+                shutil.copyfileobj(f_zipped, f_unzipped)
+        logger.info('Unzipped model.')
+
+    lm_path = lm_dir / ('lowercase_' + str(lm_model))
+    if not os.path.exists(lm_path):
+        with open(uppercase_lm_path, 'r') as f_upper:
+            with open(lm_path, 'w') as f_lower:
+                for line in f_upper:
+                    f_lower.write(line.lower())
+
+    logger.info(f'Model available at {lm_path}')
+
+    return lm_path
