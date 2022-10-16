489ee74cf052e9d3eccd380572a7d678264d9bcd
fedorovgv <ivlog7@yandex.ru>
2022-10-16 16:30:22 +0300

    Fix lowercase in lm

--- hw_asr/text_encoder/language_model.py
+++ hw_asr/text_encoder/language_model.py
@@ -15,14 +15,12 @@ AVAILABLE_MODELS = [
 ]
 
 
-def load_model(lm_dir: Path, lm_model: str):
+def load_model(lm_dir: Path, lm_model: str, lm_path: str = None):
     assert lm_model in AVAILABLE_MODELS
 
     lm_gzip_path = lm_model + '.gz'
-    print(f'LM MODEL: {lm_model}  == ? 3-gram.pruned.1e-7.arpa, {os.path.exists(lm_model)}')
-    if os.path.exists(lm_model):
-        logger.info(f'We also have language model in {lm_dir}')
-    else:
+
+    if not os.path.exists(lm_model):
         logger.info(f'Downloading language model in {lm_dir}')
         lm_url = 'http://www.openslr.org/resources/11/' + lm_gzip_path
         download_file(lm_url, lm_dir / lm_model)
@@ -34,12 +32,12 @@ def load_model(lm_dir: Path, lm_model: str):
                 shutil.copyfileobj(f_zipped, f_unzipped)
         logger.info('Unzipped model.')
 
-    lm_path = lm_dir / ('lowercase_' + str(lm_model))
-    if not os.path.exists(lm_path):
-        with open(uppercase_lm_path, 'r') as f_upper:
-            with open(lm_path, 'w') as f_lower:
-                for line in f_upper:
-                    f_lower.write(line.lower())
+    # lm_path = lm_dir / ('lowercase_' + str(lm_model))
+    # if not os.path.exists(lm_path):
+    #     with open(uppercase_lm_path, 'r') as f_upper:
+    #         with open(lm_path, 'w') as f_lower:
+    #             for line in f_upper:
+    #                 f_lower.write(line.lower())
 
     logger.info(f'Model available at {lm_path}')
 
